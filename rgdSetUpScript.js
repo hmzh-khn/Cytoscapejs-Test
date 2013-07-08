a0c4d9fd0508757b61e63b76239a47a2c4fb79a8
Hamzah Khan <khanh@catlin.edu>
2013-07-08 10:09:31 -0700

    Fix naming errors
    Remove uncreated object dataXHR
    Rename dashSplit to _Split

--- rgdSetUpScript.js
+++ rgdSetUpScript.js
@@ -2,11 +2,13 @@ var rgdMap = {},
   dataURL = 'http://cs.catlin.edu/~khanh/cytoscape/RGD_ORTHOLOGS.txt',
   lineSplit = /\n/,
   spaceSplit = ' ', //could be used depending on whcih file i am using server is space
-  dashSplit = '_';
+  _Split = '_';
 
-$.get(dataURL, {}, function(responseText){  
-  var data = dataXHR.responseText.split(lineSplit);    //split by new line
+$.get(dataURL, {}, function(responseText){ 
+  console.log(responseText); 
+  var data = responseText.split(lineSplit);    //split by new line
   var lineNum = 1;
+  console.log(data);
 
   for(var lineNum = 1; lineNum < data.length - 1; lineNum++) {
     var dataArr = data[lineNum-1].split(_Split);  //split by tabs
@@ -21,7 +23,7 @@ $.get(dataURL, {}, function(responseText){
 
     rgdMap[dataArray[0]] = new RGD(dataArray, lineNum);  //new rgd's with rat as key
   }; 
-},'text');  
+},'jsonp');  
 
 /*
 $.ajax({

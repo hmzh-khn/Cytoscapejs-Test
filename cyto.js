5e6ee1c629c700428a9ec5beebf085190aa5126d
Hamzah Khan <khanh@catlin.edu>
2013-07-10 12:28:28 -0700

    Add links to mouse and rat data

--- cyto.js
+++ cyto.js
@@ -12,7 +12,7 @@ var renderCyto = function renderCyto(cytoVar) {
   _.each(cytoInfo.links, function(link) {
       cLinks.push({data: link.data});
   });
-  
+
   $('#cy').cytoscape({
     style: cytoscape.stylesheet()
       .selector('core')
@@ -58,10 +58,19 @@ var renderCyto = function renderCyto(cytoVar) {
       cy.on('tap', 'node', function(e){
         var node = e.cyTarget; 
         var neighborhood = node.neighborhood().add(node);
-        var nodeInfo = rgdMap[node.id()];
+        var nodeInfo = rgdMap[node.id()]; //change this to use built in data
         
         msgBox.innerHTML = node.data('id') + ' : ' + node.data('name') + '\n';
-        msgBox.innerHTML += "<a href='" + geneInfoLink + nodeInfo.human.entrezGeneId + "'>Additional Information</a>"
+        
+        if(nodeInfo.human) {
+          msgBox.innerHTML += "<a target='_blank' href='" + geneInfoLink + nodeInfo.human.entrezGeneId + "'>Additional Human Information</a><br>";
+        }
+        if(nodeInfo.rat) {
+          msgBox.innerHTML += "<a target='_blank' href='" + geneInfoLink + nodeInfo.rat.entrezGeneId + "'>Additional Rat Information</a><br>";
+        }
+        if(nodeInfo.mouse) {
+          msgBox.innerHTML += "<a target='_blank' href='" + geneInfoLink + nodeInfo.mouse.entrezGeneId + "'>Additional Mouse Information</a><br>";
+        }
 
         cy.elements().addClass('faded');
         neighborhood.removeClass('faded');
